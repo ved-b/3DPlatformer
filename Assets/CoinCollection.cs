a567e3f221551badb7711687b0ac7094e3974228
ved-b <90875244+ved-b@users.noreply.github.com>
2025-03-02 16:36:52 -0800

    Added coin collection script

--- Assets/CoinCollection.cs
+++ Assets/CoinCollection.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class CoinCollection : MonoBehaviour
 {
     private int coin = 0;
 
+    public TextMeshProUGUI coinText;
+
     private void OnTriggerEnter(Collider other){
 
         if(other.transform.tag == "Coin"){
             coin++;
+            coinText.text = "Coins: " + coin.ToString();
             Debug.Log("Coin collected: " + coin);
             Destroy(other.gameObject);
+            // Bringing loader back
         }
     }
 }
